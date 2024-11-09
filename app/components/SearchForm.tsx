f4745a53f3e9429662253feb6d09cbe2b01c0ba8
Takhir <guvalakat1603@gmail.com>
2024-11-09 22:28:04 +0500

    SearchFormReset.tsx component is used

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import Form from "next/form";
+import SearchFormReset from "@/app/components/SearchFormReset";
 
 const SearchForm = () => {
+    const query = "Test";
+
     return (
-        <div>SearchForm</div>
+        <Form action={"/"} scroll={false} className={"search-form"}>
+            <input
+                name={"query"}
+                defaultValue={query}
+                type="text"
+                className={"search-input"}
+                placeholder={"Search Startups"}
+            />
+            <div className="flex gap-2">
+                {query && <SearchFormReset />}
+
+                <button type={"submit"} className={"search-btn text-white"}>S</button>
+            </div>
+        </Form>
     )
 }
 export default SearchForm
