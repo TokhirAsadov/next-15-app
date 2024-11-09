db394ef52e1fc902ce27d2fe86f366618cd6b14a
Takhir <guvalakat1603@gmail.com>
2024-11-09 13:19:28 +0500

    pink_container and sub-heading is done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-        <h1 className="heading">Home</h1>
+        <section className="pink_container">
+            <h1 className="heading">Pitch Your Startup, <br/> Connect With Entrepreneurs</h1>
+            <p className="sub-heading !max-w-3xl">
+                Submit Ideas, Vote on Pitches, and Get Noticed In Virtual Competitions.
+            </p>
+        </section>
     </>
   );
 }
