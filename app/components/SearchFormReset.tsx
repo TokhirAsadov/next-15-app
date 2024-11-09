3abefc3857c33c629d31abb68fbfba840d8e4a4c
Takhir <guvalakat1603@gmail.com>
2024-11-09 22:27:58 +0500

    SearchFormReset.tsx component is done

--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+"use client"
+
+import Link from "next/link";
 
 const SearchFormReset = () => {
+
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement;
+        if(form) form.reset();
+    }
+
     return (
-        <div>SearchFormReset</div>
+        <button type={"reset"} onClick={reset}>
+            <Link href={"/"} className={"search-btn text-white"}>
+                X
+            </Link>
+        </button>
     )
 }
 export default SearchFormReset
