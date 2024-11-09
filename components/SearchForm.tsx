0714b0b09304d4eda2d81bc11e12e184497c6328
Takhir <guvalakat1603@gmail.com>
2024-11-09 22:39:05 +0500

    page is changed

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,9 +1,8 @@
 import Form from "next/form";
-import SearchFormReset from "@/app/components/SearchFormReset";
+import SearchFormReset from "@/components/SearchFormReset";
+import {Search} from "lucide-react";
 
-const SearchForm = () => {
-    const query = "Test";
-
+const SearchForm = ({ query } : {query?: string}) => {
     return (
         <Form action={"/"} scroll={false} className={"search-form"}>
             <input
@@ -16,7 +15,9 @@ const SearchForm = () => {
             <div className="flex gap-2">
                 {query && <SearchFormReset />}
 
-                <button type={"submit"} className={"search-btn text-white"}>S</button>
+                <button type={"submit"} className={"search-btn text-white"}>
+                    <Search className={"size-5"}/>
+                </button>
             </div>
         </Form>
     )
