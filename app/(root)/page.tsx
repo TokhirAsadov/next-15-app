fbad0602c3193bbbd12361ce8d65a0d3777d09bd
Takhir <guvalakat1603@gmail.com>
2024-11-10 19:51:42 +0500

    StartupCard.tsx is used

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams } : {
     searchParams: Promise<{query?: string}>
 }) {
     const query = (await searchParams).query;
 
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: { _id: 1, name: "Tokhir Asadov" },
+        _id:1,
+        description: "This is a description",
+        image: "https://www.elcom.com.vn/storage/uploads/images/4Z2zyaQRuYa81t6wuUarkozbGJJiIJSb1Ph3rfGh.jpg",
+        category: "Robots",
+        title: "We Robots",
+    }]
   return (
     <>
         <section className="pink_container">
@@ -13,6 +24,21 @@ export default async function Home({ searchParams } : {
                 Submit Ideas, Vote on Pitches, and Get Noticed In Virtual Competitions.
             </p>
             <SearchForm query={query} />
+        </section>
+        
+        <section className="section_container">
+            <p className="text-30 font-semibold">
+                {query ? `Search results for ${query}` : 'All Startups'}
+            </p>
+            <ul className="mt-7 card_grid">
+                {posts?.length > 0 ? (
+                    posts.map((post: StartupCardType) => (
+                        <StartupCard post={post} key={post?.id}/>
+                    ))
+                ) : (
+                    <p className="no-result">No startups found</p>
+                )}
+            </ul>
         </section>
     </>
   );
