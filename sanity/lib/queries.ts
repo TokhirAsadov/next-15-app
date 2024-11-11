7501613a28ca4d94ae153401d372a540f78d64fc
Takhir <guvalakat1603@gmail.com>
2024-11-12 02:27:20 +0500

    STARTUP_QUERY is written

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,2 +1,20 @@
 import {defineQuery} from "groq";
 
+export const STARTUP_QUERY = defineQuery(`*[_type=="startup" && defined(slug.current)] | order(_createdAt desc) {
+  _id, 
+    title, 
+    slug, 
+    _createdAt, 
+    author -> {
+       _id, 
+      name, 
+      username, 
+      image, 
+      bio 
+    }, 
+    views, 
+    description, 
+    category, 
+    pitch,
+    image
+}`)
