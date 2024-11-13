72526cf7c1b8536475f310cbf5e7ffda0099e07d
Takhir <guvalakat1603@gmail.com>
2024-11-13 12:42:27 +0500

    session is got

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,12 +3,17 @@ import StartupCard,{StartupTypeCard} from "@/components/StartupCard";
 import {client} from "@/sanity/lib/client";
 import {STARTUP_QUERY} from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {auth} from "@/auth";
 
 export default async function Home({ searchParams } : {
     searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+
+  const session =  await auth();
+  console.log(session?.id,"id-> ")
+
   // const posts = await client.fetch(STARTUP_QUERY);
   const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params })
 
