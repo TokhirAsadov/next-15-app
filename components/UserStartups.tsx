ddc87f57f01624be14c48b836baec01e9e43ab2d
Takhir <guvalakat1603@gmail.com>
2024-11-15 21:58:57 +0500

    StartupCardSkeleton is used

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {client} from "@/sanity/lib/client";
+import {STARTUPS_BY_AUTHOR_QUERY} from "@/sanity/lib/queries";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 
-const UserStartups = () => {
+const UserStartups = async ({ id } : { id: string }) => {
+    const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY,{ id })
+
     return (
-        <div>UserStartups</div>
+        <>
+            {startups.length > 0 ? startups.map((startup: StartupTypeCard) => (
+                <StartupCard post={startup} key={startup._id} />
+            )) : (
+                <p className="no-result">No posts yet</p>
+            )}
+        </>
     )
 }
 export default UserStartups
