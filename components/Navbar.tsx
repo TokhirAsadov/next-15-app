9f9ed504ff1de1c4bfd1c452ac34156670e4dcf3
Takhir <guvalakat1603@gmail.com>
2024-11-15 17:11:22 +0500

    changing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import {auth, signIn, signOut} from "@/auth";
 
 const Navbar = async () => {
-    const  session = await auth()
+    const session = await auth();
+    console.log(session)
     return (
         <div className={"px-5 py-3 bg-white shadow-sm font-work-sans"}>
             <nav className={"flex justify-between items-center"}>
@@ -27,7 +28,7 @@ const Navbar = async () => {
                                 <button type={"submit"}>Logout</button>
                             </form>
 
-                            <Link href={`/user/${session?.user?.id}`}>
+                            <Link href={`/user/${session?.id}`}>
                                 <span>{session?.user?.name}</span>
                             </Link>
                         </>
