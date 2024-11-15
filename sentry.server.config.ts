fcd3013ad7a7b565fbf22d39f3aed4654079850a
Takhir <guvalakat1603@gmail.com>
2024-11-15 16:23:24 +0500

    Sentry debug monitoring controller

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://66041413bee4722a67ea53cff394f04c@o4508301845069824.ingest.de.sentry.io/4508301851623504",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
