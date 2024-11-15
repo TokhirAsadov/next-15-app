9b67bf1e44731c25bcea9ead108bbeb05ee0de6f
Takhir <guvalakat1603@gmail.com>
2024-11-15 21:58:51 +0500

    StartupCardSkeleton is created

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {formatDate} from "@/lib/utils";
+import {cn, formatDate} from "@/lib/utils";
 import {EyeIcon} from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {Author, Startup} from "@/sanity.types";
+import {Skeleton} from "@/components/ui/skeleton";
 
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
@@ -67,4 +68,15 @@ const StartupCard = ({ post } : { post: StartupTypeCard}) => {
         </li>
     )
 }
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0,1,2,3,4].map(( i: number) => (
+                <li key={cn('skeleton',i)}>
+                    <Skeleton className={"startup-card_skeleton"} />
+                </li>
+            ))}
+    </>
+)
+
 export default StartupCard
