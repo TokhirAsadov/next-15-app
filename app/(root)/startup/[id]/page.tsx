4be0b70bd0fc362d498f7cc249a30bafd368a924
Takhir <guvalakat1603@gmail.com>
2024-11-16 20:52:10 +0500

    PLAYLIST_BY_SLUG_QUERY is used

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React, {Suspense} from 'react'
 import {client} from "@/sanity/lib/client";
-import {STARTUP_BY_ID_QUERY} from "@/sanity/lib/queries";
+import {PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY} from "@/sanity/lib/queries";
 import {notFound} from "next/navigation";
 import {formatDate} from "@/lib/utils";
 import Link from "next/link";
@@ -8,6 +8,7 @@ import Image from "next/image";
 import markdownit from 'markdown-it';
 import {Skeleton} from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 
 
 export const experimental_ppr = true;
@@ -16,6 +17,9 @@ const Page = async ({ params } : { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
 
     const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+    const { select: editorPosts } = await client
+        .fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'java-basic-knowledge' });
 
     if(!post) return notFound();
 
@@ -70,7 +74,17 @@ const Page = async ({ params } : { params: Promise<{ id: string }> }) => {
 
                 <hr className="divider" />
 
-                {/*  TODO: EDITOR SELECTED STARTUPS  */}
+                {editorPosts?.length > 0 && (
+                    <div className={"max-w-4xl mx-auto"}>
+                        <p className="text-30-semibold">Editor Picks</p>
+
+                        <ul className={"mt-7 card_grid-sm"}>
+                            {editorPosts.map((post: StartupTypeCard, index: number) => (
+                                <StartupCard post={post} key={index} />
+                            ))}
+                        </ul>
+                    </div>
+                )}
 
                 <Suspense fallback={<Skeleton className={"view_skeleton"}/>}>
                     <View id={id}/>
